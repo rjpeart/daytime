5f774d96c443d1d34355674437d810bc84239a03
Rob Peart <rjpeart@gmail.com>
2024-10-08 15:30:47 +0100

    added mobile support, and added loader animation. Also added geolocation support

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var pixelThreshold = 0;
 var lat;
 var long;
 var apiUrl;
+var city;
+var country;
 
 // Function to fetch data from an API
 function fetchData(url) {
@@ -24,42 +26,56 @@ function fetchData(url) {
         })
         .then(data => {
             console.log('Data received:', data);
+            if (!data.results || !data.results.sunrise || !data.results.sunset) {
+                throw new Error('API response is missing expected data');
+            }
             sunrise = convertTimeFormat(data.results.sunrise);
+            console.log('Converted sunrise:', sunrise);
             sunset = convertTimeFormat(data.results.sunset);
+            console.log('Converted sunset:', sunset);
             dayhours = calculateTimeDifference(sunrise, sunset);
+            console.log('Calculated dayhours:', dayhours);
             nighthours = calculateTimeDifference(sunset, sunrise);
+            console.log('Calculated nighthours:', nighthours);
             daylight = timeToPercentage(dayhours)
+            console.log('Calculated daylight percentage:', daylight);
             nighttime = 100-daylight;
-            console.log(nighttime)
+            console.log('Calculated nighttime:', nighttime);
             adjustWidth(daylight, nighttime)
-
             calculatePixelThreshold();
-
             const loading = document.getElementById('loading');
             loading.style.display = "none";
-
             window.addEventListener("mousemove", (e) => {
                 moveInfo(e);
             });
-
-            
-            
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
+            // Handle the error appropriately (e.g., display an error message to the user)
         });
 }
 
 function convertTimeFormat(timeString) {
-    // Create a Date object from the time string
-    const date = new Date(`1970-01-01 ${timeString}`);
-    
+    // Check if the timeString is in the expected format
+    if (!/^\d{1,2}:\d{2}:\d{2}\s[AP]M$/.test(timeString)) {
+        console.error('Invalid time format:', timeString);
+        return "00:00";
+    }
+
+    // Split the time string into components
+    const [time, period] = timeString.split(' ');
+    let [hours, minutes] = time.split(':');
+
+    // Convert to 24-hour format
+    hours = parseInt(hours);
+    if (period === 'PM' && hours !== 12) {
+        hours += 12;
+    } else if (period === 'AM' && hours === 12) {
+        hours = 0;
+    }
+
     // Format the hours and minutes
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    
-    // Return the formatted time
-    return `${hours}:${minutes}`;
+    return `${hours.toString().padStart(2, '0')}:${minutes}`;
 }
 
 function calculateTimeDifference(startTime, endTime) {
@@ -101,82 +117,128 @@ function timeToPercentage(timeString) {
     return Math.round(percentage * 100) / 100;
 }
 
-function adjustWidth(dayWidth, nightWidth){
+function adjustWidth(dayPercentage, nightPercentage) {
     const dayDiv = document.getElementById('day');
     const nightDiv = document.getElementById('night');
 
-    dayDiv.style.width = dayWidth+"svw"
-    nightDiv.style.width = nightWidth+"svw"
-
+    if (window.innerWidth <= 768) {
+        dayDiv.style.height = dayPercentage + "svh";
+        nightDiv.style.height = nightPercentage + "svh";
+        dayDiv.style.width = "100%";
+        nightDiv.style.width = "100%";
+    } else {
+        dayDiv.style.width = dayPercentage + "svw";
+        nightDiv.style.width = nightPercentage + "svw";
+        dayDiv.style.height = "100%";
+        nightDiv.style.height = "100%";
+    }
 }
 
-function calculatePixelThreshold(){
-    windowWidth = window.innerWidth;
-    console.log(daylight)
-    var daylightMultiplier = daylight*0.01;
-    pixelThreshold = windowWidth * daylightMultiplier
-    console.log(pixelThreshold)
+function calculatePixelThreshold() {
+    if (window.innerWidth <= 768) {
+        windowHeight = window.innerHeight;
+        var daylightMultiplier = daylight * 0.01;
+        pixelThreshold = windowHeight * daylightMultiplier;
+    } else {
+        windowWidth = window.innerWidth;
+        var daylightMultiplier = daylight * 0.01;
+        pixelThreshold = windowWidth * daylightMultiplier;
+    }
+    console.log(pixelThreshold);
 }
 
 function moveInfo(e) {
-    x = e.pageX,
-    y = e.pageY
-	TweenLite.to(info, 0.3, {
-    css: {
-      left: x,
-      top: y
+    if (window.innerWidth <= 768) {
+        const mobInfoElements = document.getElementsByClassName('mob_info');
+        console.log("butthole"+mobInfoElements)
+        for (var i=0; i< mobInfoElements.length; i++) {
+            mobInfoElements[i].style.display = "block";
+        }
+        // Don't move the info div on mobile
+        return;    
     }
-  });
-  updateInfo();
+    const mobInfoElements = document.getElementsByClassName('mob_info');
+    const length = mobInfoElements.length;
+    for (let i = 0; i < length; i++) {
+        mobInfoElements[i].style.display = "none";
+    }
+    x = e.pageX;
+    y = e.pageY;
+    TweenLite.to(info, 0.3, {
+        css: {
+            left: x,
+            top: y
+        }
+    });
+    updateInfo();
 }
 
 function updateInfo(){
+    if (window.innerWidth <= 768) {
+
+    }
+
     if(x<pixelThreshold){
         info.style.color = "#ffffff"
-        info.innerHTML = `<p><span>&#9789;</span></p><p>Today</p><p>${nighthours} hours of night</p><p>${nighttime}%</p><p class='small'> Lat ${lat}, Long ${long}</p>`               
+        info.innerHTML = `<p><span>&#9789;</span></p><p>Today</p><p>${nighthours} hours of night</p><p>${nighttime}%</p><p class='small'> Location: ${city}, ${country}</p>`               
     } else {
             info.style.color = "#000000"
-            info.innerHTML = `<p><span>&#9737;</span></p><p>Today</p><p>${dayhours} hours of day</p><p>${daylight}%</p><p class='small'> Lat ${lat}, Long ${long}</p>`               
+            info.innerHTML = `<p><span>&#9737;</span></p><p>Today</p><p>${dayhours} hours of day</p><p>${daylight}%</p><p class='small'> Location: ${city}, ${country}</p>`               
         }
     }
 
-// function getGeo(){
+function getGeo(){
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          function(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+            lat = latitude;
+            long = longitude;
+            getCityFromCoordinates();
+            apiUrl = "https://api.sunrisesunset.io/json?lat=" + lat + "&lng=" + long + '/"'
+            fetchData(apiUrl);
+
+          },
+          function(error) {
+            console.error("Error: " + error.message);
+            lat = 51.383741;
+            long = -2.377120;
+            getCityFromCoordinates();
+            apiUrl = "https://api.sunrisesunset.io/json?lat=" + lat + "&lng=" + long + '/"'
+            fetchData(apiUrl);
+          }
+        );
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+      }
+}
+
+// Function to perform reverse geocoding
+function getCityFromCoordinates(latitude, longitude) {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${long}`;
     
-//     if ("geolocation" in navigator) {
-//         navigator.geolocation.getCurrentPosition(function(position) {
-//             console.log("getting")
-//             lat = position.coords.latitude;
-//             long = position.coords.longitude;
-//             // apiUrl = "https://api.sunrisesunset.io/json?lat=" + lat + "&lng=" + long + '"'
-//             apiUrl="https://api.sunrisesunset.io/json?lat=38.907192&lng=-77.036873/"
-//             console.log(apiUrl);
-//             fetchData(apiUrl);
-//         }, function(error) {
-//             console.log("getting")
-//           lat = 51.383741;
-//           long = -2.377120;
-//           apiUrl = "https://api.sunrisesunset.io/json?lat=" + lat + "&lng=" + long + '/"'
-//           fetchData(apiUrl);
-//           console.error("Error: " + error.message);
-//         });
-//       } else {
-//         console.log("getting")
-//         console.log("Geolocation is not supported by this browser.");
-//         lat = 51.383741;
-//         long = -2.377120;
-//         apiUrl = "https://api.sunrisesunset.io/json?lat=" + lat + "&lng=" + long + '/"'
-//         fetchData(apiUrl);
-//       }
-// }
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        city = data.address.city || data.address.town || data.address.village || "City not found";
+        country = data.address.country || "Country not found";
+        console.log(`City: ${city}, ${country}`); 
+      })
+      .catch(error => {
+        console.error("Error fetching city:", error);
+      });
+  }
 
 window.onload = (event) => {
     info = document.getElementById('info');
-    // getGeo();
-            lat = 51.383741;
-          long = -2.377120;
-          apiUrl = "https://api.sunrisesunset.io/json?lat=" + lat + "&lng=" + long + '/"'
-          fetchData(apiUrl);
+    getGeo();
   };
 
+  window.addEventListener('resize', () => {
+    adjustWidth(daylight, nighttime);
+    calculatePixelThreshold();
+});
 
 
---
+++ script2.js
@@ -0,0 +1,250 @@
+// Constants and configuration
+const DEFAULT_LOCATION = { lat: 51.383741, long: -2.377120 };
+const API_URL = "https://api.sunrisesunset.io/json";
+const NOMINATIM_URL = "https://nominatim.openstreetmap.org/reverse";
+
+// State variables
+let state = {
+    sunrise: '',
+    sunset: '',
+    daylight: 0,
+    nighttime: 0,
+    dayhours: '',
+    nighthours: '',
+    city: '',
+    country: '',
+    pixelThreshold: 0
+};
+
+// DOM elements
+const elements = {
+    info: null,
+    loading: null,
+    night: null,
+    day: null,
+    mobInfoNight: null,
+    mobInfoDay: null
+};
+
+// Initialization
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    cacheElements();
+    getGeolocation();
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('mousemove', handleMouseMove);
+}
+
+function cacheElements() {
+    elements.info = document.getElementById('info');
+    elements.loading = document.getElementById('loading');
+    elements.night = document.getElementById('night');
+    elements.day = document.getElementById('day');
+    elements.mobInfoNight = document.getElementById('mobInfo_night');
+    elements.mobInfoDay = document.getElementById('mobInfo_day');
+    elements.loadingAnimation = document.getElementById('loading-animation');
+
+}
+
+// Geolocation and API calls
+function getGeolocation() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            position => fetchSunData(position.coords),
+            error => {
+                console.error("Geolocation error:", error.message);
+                fetchSunData(DEFAULT_LOCATION);
+            }
+        );
+    } else {
+        console.log("Geolocation not supported");
+        fetchSunData(DEFAULT_LOCATION);
+    }
+}
+
+function fetchSunData({ lat, long }) {
+    const url = `${API_URL}?lat=${lat}&lng=${long}`;
+    fetchData(url)
+        .then(data => {
+            if (!data.results || !data.results.sunrise || !data.results.sunset) {
+                throw new Error('API response is missing expected data');
+            }
+            updateState(data.results);
+            return fetchCityData(lat, long);
+        })
+        .then(() => {
+            updateUI();
+            elements.loadingAnimation.style.display = "none";
+            if(window.innerWidth > 768){
+                elements.info.style.display = "block";
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            elements.loadingAnimation.querySelector('.loading-text').textContent = "Error loading data. Please try again.";
+        });
+}
+
+function fetchCityData(lat, long) {
+    const url = `${NOMINATIM_URL}?format=json&lat=${lat}&lon=${long}`;
+    return fetchData(url)
+        .then(data => {
+            state.city = data.address.city || data.address.town || data.address.village || "City not found";
+            state.country = data.address.country || "Country not found";
+        });
+}
+
+function fetchData(url) {
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not ok');
+            return response.json();
+        });
+}
+
+// State and UI updates
+function updateState(results) {
+    state.sunrise = convertTimeFormat(results.sunrise);
+    state.sunset = convertTimeFormat(results.sunset);
+    state.dayhours = calculateTimeDifference(state.sunrise, state.sunset);
+    state.nighthours = calculateTimeDifference(state.sunset, state.sunrise);
+    state.daylight = timeToPercentage(state.dayhours);
+    state.nighttime = 100 - state.daylight;
+    calculatePixelThreshold();
+}
+
+function updateUI() {
+    adjustWidth();
+    if (window.innerWidth <= 768) {
+        updateMobileInfo();
+        elements.mobInfoNight.style.display = "block";
+        elements.mobInfoDay.style.display = "block";
+        elements.info.style.display = "none";
+    } else {
+        updateInfo();
+        elements.info.style.display = "block";
+        elements.mobInfoNight.style.display = "none";
+        elements.mobInfoDay.style.display = "none";
+    }
+}
+
+// Utility functions
+function convertTimeFormat(timeString) {
+    if (!/^\d{1,2}:\d{2}:\d{2}\s[AP]M$/.test(timeString)) {
+        console.error('Invalid time format:', timeString);
+        return "00:00";
+    }
+    const [time, period] = timeString.split(' ');
+    let [hours, minutes] = time.split(':');
+    hours = parseInt(hours);
+    if (period === 'PM' && hours !== 12) hours += 12;
+    else if (period === 'AM' && hours === 12) hours = 0;
+    return `${hours.toString().padStart(2, '0')}:${minutes}`;
+}
+
+function calculateTimeDifference(startTime, endTime) {
+    const [startHours, startMinutes] = startTime.split(':').map(Number);
+    const [endHours, endMinutes] = endTime.split(':').map(Number);
+    let startTotalMinutes = startHours * 60 + startMinutes;
+    let endTotalMinutes = endHours * 60 + endMinutes;
+    if (endTotalMinutes < startTotalMinutes) endTotalMinutes += 24 * 60;
+    let differenceMinutes = endTotalMinutes - startTotalMinutes;
+    const hours = Math.floor(differenceMinutes / 60);
+    const minutes = differenceMinutes % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+}
+
+function timeToPercentage(timeString) {
+    const [hours, minutes] = timeString.split(':').map(Number);
+    const totalMinutes = hours * 60 + minutes;
+    return Math.round((totalMinutes / 1440) * 10000) / 100;
+}
+
+function calculatePixelThreshold() {
+    const dimension = window.innerWidth <= 768 ? window.innerHeight : window.innerWidth;
+    state.pixelThreshold = dimension * (state.nighttime / 100); // Changed from daylight to nighttime
+}
+
+// Event handlers
+function handleResize() {
+    adjustWidth();
+    calculatePixelThreshold();
+    updateUI(); // This will handle showing/hiding the correct info based on screen size
+}
+
+function handleMouseMove(e) {
+    if (window.innerWidth <= 768) {
+        // Do nothing for mobile view
+    } else {
+        moveInfoElement(e.pageX, e.pageY);
+        updateInfo(e.pageX);
+    }
+}
+
+// UI manipulation
+function adjustWidth() {
+    const { daylight, nighttime } = state;
+    if (window.innerWidth <= 768) {
+        elements.day.style.height = `${daylight}svh`;
+        elements.night.style.height = `${nighttime}svh`;
+        elements.day.style.width = elements.night.style.width = "100%";
+    } else {
+        elements.day.style.width = `${daylight}svw`;
+        elements.night.style.width = `${nighttime}svw`;
+        elements.day.style.height = elements.night.style.height = "100%";
+    }
+}
+
+
+function moveInfoElement(x, y) {
+    const infoWidth = elements.info.offsetWidth;
+    const infoHeight = elements.info.offsetHeight;
+    const maxX = window.innerWidth - infoWidth;
+    const maxY = window.innerHeight - infoHeight;
+
+    // Ensure the info element stays within the viewport
+    const adjustedX = Math.min(Math.max(0, x), maxX);
+    const adjustedY = Math.min(Math.max(0, y), maxY);
+
+    TweenLite.to(elements.info, 0.3, { css: { left: adjustedX, top: adjustedY } });
+}
+
+function updateInfo(mouseX) {
+    const { dayhours, nighthours, daylight, nighttime, city, country } = state;
+    const isDaytime = mouseX < state.pixelThreshold;
+
+    // Set text color based on background
+    const backgroundColor = isDaytime ? "#ffffff" : "#000000";
+    const textColor = isDaytime ? "#ffffff" : "#000000";
+
+    elements.info.style.color = textColor;
+
+    elements.info.innerHTML = `
+        <p><span>${isDaytime ? '☀' : '☾'}</span></p>
+        <p>Today</p>
+        <p>${isDaytime ? dayhours : nighthours} hours of ${isDaytime ? 'day' : 'night'}</p>
+        <p>${isDaytime ? daylight : nighttime}%</p>
+        <p class='small'>Location: ${city}, ${country}</p>
+    `;
+}
+
+function updateMobileInfo() {
+    const { dayhours, nighthours, daylight, nighttime, city, country } = state;
+    
+    elements.mobInfoDay.innerHTML = `
+        <p><span>☀</span></p>
+        <p>Today</p>
+        <p>${dayhours} hours of day</p>
+        <p>${daylight}%</p>
+        <p class='small'>Location: ${city}, ${country}</p>
+    `;
+
+    elements.mobInfoNight.innerHTML = `
+        <p><span>☾</span></p>
+        <p>Today</p>
+        <p>${nighthours} hours of night</p>
+        <p>${nighttime}%</p>
+        <p class='small'>Location: ${city}, ${country}</p>
+    `;
+}
