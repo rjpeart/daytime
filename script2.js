63dee446951e5ec5c779f93980a622cee0736dcc
Rob Peart <rjpeart@gmail.com>
2024-10-09 10:02:23 +0100

    removed html5 GPS, added IP based locating

--- script2.js
+++ script2.js
@@ -37,17 +37,25 @@ const elements = {
     night: null,
     day: null,
     mobInfoNight: null,
-    mobInfoDay: null
+    mobInfoDay: null,
+    loadingAnimation: null,
+    citySelect: null
 };
 
 // Initialization
 document.addEventListener('DOMContentLoaded', init);
 
-function init() {
-    cacheElements();
+async function init() {
     createCityDropdown();
-    getGeolocation();
-    // fetchSunData(DEFAULT_LOCATION);
+    cacheElements();
+    try {
+        const coords = await getIPBasedLocation();
+        await fetchSunData(coords);
+        updateDropdownWithNearestCity(coords);
+    } catch (error) {
+        console.error("Error during initialization:", error);
+        handleLocationError();
+    }
     window.addEventListener('resize', handleResize);
     window.addEventListener('mousemove', handleMouseMove);
 }
@@ -61,94 +69,57 @@ function cacheElements() {
     elements.mobInfoDay = document.getElementById('mobInfo_day');
     elements.loadingAnimation = document.getElementById('loading-animation');
     elements.citySelect = document.getElementById('city-select');
-
-
 }
 
-// Create and populate the dropdown
-function createCityDropdown() {
-    const select = document.createElement('select');
-    select.id = 'city-select';
-    select.innerHTML = '<option value="">Select a city</option>';
-    MAJOR_CITIES.forEach(city => {
-        const option = document.createElement('option');
-        option.value = JSON.stringify({lat: city.lat, long: city.long});
-        option.textContent = city.name;
-        select.appendChild(option);
-    });
-    select.addEventListener('change', handleCitySelection);
-    document.body.appendChild(select);
-}
-
-// Handle city selection
-function handleCitySelection(event) {
-    if (event.target.value) {
-        const coords = JSON.parse(event.target.value);
-        fetchSunData(coords);
+// Location functions
+async function getIPBasedLocation() {
+    try {
+        const response = await fetch('https://ipapi.co/json/');
+        const data = await response.json();
+        if (!data.latitude || !data.longitude) {
+            throw new Error("Invalid location data from IP geolocation");
+        }
+        console.log("lat= " + data.latitude + " long= " + data.longitude)
+        return { lat: data.latitude, long: data.longitude };
+    } catch (error) {
+        console.error("Error fetching IP-based location:", error);
+        throw error;
     }
 }
 
-function getGeolocation() {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                fetchSunData(position.coords);
-                updateDropdownWithNearestCity(position.coords);
-            },
-            error => {
-                console.warn("Geolocation error:", error.message);
-                fallbackToIPGeolocation();
-            }
-        );
-    } else {
-        console.log("Geolocation not supported");
-        fallbackToIPGeolocation();
-    }
-}
-
-function fallbackToIPGeolocation() {
-    fetch('https://ipapi.co/json/')
-        .then(response => response.json())
-        .then(data => {
-            const location = { lat: data.latitude, long: data.longitude };
-            fetchSunData(location);
-            updateDropdownWithNearestCity(location);
-        })
-        .catch(error => {
-            console.error("Error fetching IP-based location:", error);
-            useDefaultLocation();
-        });
-}
-
-function useDefaultLocation() {
-    console.log("Using default location");
-    const defaultLocation = MAJOR_CITIES[0];  // Using the first city as default
-    fetchSunData(defaultLocation);
-    elements.citySelect.value = JSON.stringify({lat: defaultLocation.lat, long: defaultLocation.long});
-}
-
-function fetchSunData({ lat, long }) {
-    const url = `${API_URL}?lat=${lat}&lng=${long}`;
-    fetchData(url)
-        .then(data => {
-            if (!data.results || !data.results.sunrise || !data.results.sunset) {
-                throw new Error('API response is missing expected data');
-            }
-            updateState(data.results);
-            return fetchCityData(lat, long);
-        })
+function handleLocationError() {
+    console.log("Using default location due to error");
+    const defaultCity = MAJOR_CITIES[0];
+    fetchSunData(defaultCity)
         .then(() => {
-            updateUI();
-            elements.loadingAnimation.style.display = "none";
-            elements.citySelect.style.display = "block";  // Show the dropdown
-            if(window.innerWidth > 768){
-                elements.info.style.display = "block";
-            }
+            elements.citySelect.value = JSON.stringify({lat: defaultCity.lat, long: defaultCity.long});
         })
-        .catch(error => {
-            console.error('Error:', error);
-            elements.loadingAnimation.querySelector('.loading-text').textContent = "Error loading data. Please try again.";
-        });
+        .catch(handleAPIError);
+}
+
+//API Functions
+async function fetchSunData({ lat, long }) {
+    if (!lat || !long) {
+        throw new Error("Invalid coordinates");
+    }
+    const url = `${API_URL}?lat=${lat}&lng=${long}`;
+    try {
+        const data = await fetchData(url);
+        if (!data.results || !data.results.sunrise || !data.results.sunset) {
+            throw new Error('API response is missing expected data');
+        }
+        updateState(data.results);
+        await fetchCityData(lat, long);
+        updateUI();
+        elements.loadingAnimation.style.display = "none";
+        elements.citySelect.style.display = "block";
+        if (window.innerWidth > 768) {
+            elements.info.style.display = "block";
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        handleAPIError(error);
+    }
 }
 
 function fetchCityData(lat, long) {
@@ -168,7 +139,14 @@ function fetchData(url) {
         });
 }
 
-// State and UI updates
+function handleAPIError(error) {
+    console.error('API Error:', error);
+    elements.loadingAnimation.style.display = "none";
+    elements.citySelect.style.display = "block";
+    alert("Error loading data. Please try selecting a city from the dropdown.");
+}
+
+//State and UI update functions
 function updateState(results) {
     state.sunrise = convertTimeFormat(results.sunrise);
     state.sunset = convertTimeFormat(results.sunset);
@@ -192,149 +170,6 @@ function updateUI() {
         elements.mobInfoNight.style.display = "none";
         elements.mobInfoDay.style.display = "none";
     }
-}
-
-function createCityDropdown() {
-    const select = document.createElement('select');
-    select.id = 'city-select';
-    
-    // Add the default "Change location" option
-    const defaultOption = document.createElement('option');
-    defaultOption.value = "";
-    defaultOption.textContent = "Change location";
-    select.appendChild(defaultOption);
-
-    // Add the city options
-    MAJOR_CITIES.forEach(city => {
-        const option = document.createElement('option');
-        option.value = JSON.stringify({lat: city.lat, long: city.long});
-        option.textContent = city.name;
-        select.appendChild(option);
-    });
-
-    select.addEventListener('change', handleCitySelection);
-    document.body.appendChild(select);
-    elements.citySelect = select;
-}
-
-function handleCitySelection(event) {
-    if (event.target.value) {
-        const coords = JSON.parse(event.target.value);
-        fetchSunData(coords);
-    }
-}
-
-function updateDropdownWithNearestCity(coords) {
-    const nearest = findNearestCity(coords);
-    const nearestCityValue = JSON.stringify({lat: nearest.lat, long: nearest.long});
-    
-    // Only update if a nearest city was found
-    // if (nearest.name) {
-    //     elements.citySelect.value = nearestCityValue;
-    // } else {
-    //     // If no nearest city found, reset to "Change location"
-    //     elements.citySelect.value = "";
-    // }
-}
-
-function findNearestCity(coords) {
-    return MAJOR_CITIES.reduce((nearest, city) => {
-        const distance = getDistance(coords, city);
-        return distance < nearest.distance ? {...city, distance} : nearest;
-    }, {distance: Infinity});
-}
-
-function getDistance(coords1, coords2) {
-    const R = 6371; // Radius of the Earth in km
-    const dLat = (coords2.lat - coords1.lat) * Math.PI / 180;
-    const dLon = (coords2.long - coords1.long) * Math.PI / 180;
-    const a = 
-        Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(coords1.lat * Math.PI / 180) * Math.cos(coords2.lat * Math.PI / 180) * 
-        Math.sin(dLon/2) * Math.sin(dLon/2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    return R * c;
-}
-
-// Utility functions
-function convertTimeFormat(timeString) {
-    if (!/^\d{1,2}:\d{2}:\d{2}\s[AP]M$/.test(timeString)) {
-        console.error('Invalid time format:', timeString);
-        return "00:00";
-    }
-    const [time, period] = timeString.split(' ');
-    let [hours, minutes] = time.split(':');
-    hours = parseInt(hours);
-    if (period === 'PM' && hours !== 12) hours += 12;
-    else if (period === 'AM' && hours === 12) hours = 0;
-    return `${hours.toString().padStart(2, '0')}:${minutes}`;
-}
-
-function calculateTimeDifference(startTime, endTime) {
-    const [startHours, startMinutes] = startTime.split(':').map(Number);
-    const [endHours, endMinutes] = endTime.split(':').map(Number);
-    let startTotalMinutes = startHours * 60 + startMinutes;
-    let endTotalMinutes = endHours * 60 + endMinutes;
-    if (endTotalMinutes < startTotalMinutes) endTotalMinutes += 24 * 60;
-    let differenceMinutes = endTotalMinutes - startTotalMinutes;
-    const hours = Math.floor(differenceMinutes / 60);
-    const minutes = differenceMinutes % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-}
-
-function timeToPercentage(timeString) {
-    const [hours, minutes] = timeString.split(':').map(Number);
-    const totalMinutes = hours * 60 + minutes;
-    return Math.round((totalMinutes / 1440) * 10000) / 100;
-}
-
-function calculatePixelThreshold() {
-    const dimension = window.innerWidth <= 768 ? window.innerHeight : window.innerWidth;
-    state.pixelThreshold = dimension * (state.nighttime / 100); // Changed from daylight to nighttime
-}
-
-// Event handlers
-function handleResize() {
-    adjustWidth();
-    calculatePixelThreshold();
-    updateUI(); // This will handle showing/hiding the correct info based on screen size
-}
-
-function handleMouseMove(e) {
-    if (window.innerWidth <= 768) {
-        // Do nothing for mobile view
-    } else {
-        moveInfoElement(e.pageX, e.pageY);
-        updateInfo(e.pageX);
-    }
-}
-
-// UI manipulation
-function adjustWidth() {
-    const { daylight, nighttime } = state;
-    if (window.innerWidth <= 768) {
-        elements.day.style.height = `${daylight}svh`;
-        elements.night.style.height = `${nighttime}svh`;
-        elements.day.style.width = elements.night.style.width = "100%";
-    } else {
-        elements.day.style.width = `${daylight}svw`;
-        elements.night.style.width = `${nighttime}svw`;
-        elements.day.style.height = elements.night.style.height = "100%";
-    }
-}
-
-
-function moveInfoElement(x, y) {
-    const infoWidth = elements.info.offsetWidth;
-    const infoHeight = elements.info.offsetHeight;
-    const maxX = window.innerWidth - infoWidth;
-    const maxY = window.innerHeight - infoHeight;
-
-    // Ensure the info element stays within the viewport
-    const adjustedX = Math.min(Math.max(0, x), maxX);
-    const adjustedY = Math.min(Math.max(0, y), maxY);
-
-    TweenLite.to(elements.info, 0.3, { css: { left: adjustedX, top: adjustedY } });
 }
 
 function updateInfo(mouseX) {
@@ -374,4 +209,132 @@ function updateMobileInfo() {
         <p>${nighttime}%</p>
         <p class='small'>Location: ${city}, ${country}</p>
     `;
+}
+
+//City dropdown functions
+// Create and populate the dropdown
+function createCityDropdown() {
+    const select = document.createElement('select');
+    select.id = 'city-select';
+    select.innerHTML = '<option value="">Select a city</option>';
+    MAJOR_CITIES.forEach(city => {
+        const option = document.createElement('option');
+        option.value = JSON.stringify({lat: city.lat, long: city.long});
+        option.textContent = city.name;
+        select.appendChild(option);
+    });
+    select.addEventListener('change', handleCitySelection);
+    document.body.appendChild(select);
+}
+
+// Handle city selection
+function handleCitySelection(event) {
+    if (event.target.value) {
+        const coords = JSON.parse(event.target.value);
+        fetchSunData(coords);
+    }
+}
+
+function updateDropdownWithNearestCity(coords) {
+    const nearest = findNearestCity(coords);
+    const nearestCityValue = JSON.stringify({lat: nearest.lat, long: nearest.long});
+}
+
+function findNearestCity(coords) {
+    return MAJOR_CITIES.reduce((nearest, city) => {
+        const distance = getDistance(coords, city);
+        return distance < nearest.distance ? {...city, distance} : nearest;
+    }, {distance: Infinity});
+}
+
+//Utility Functions
+function convertTimeFormat(timeString) {
+    if (!/^\d{1,2}:\d{2}:\d{2}\s[AP]M$/.test(timeString)) {
+        console.error('Invalid time format:', timeString);
+        return "00:00";
+    }
+    const [time, period] = timeString.split(' ');
+    let [hours, minutes] = time.split(':');
+    hours = parseInt(hours);
+    if (period === 'PM' && hours !== 12) hours += 12;
+    else if (period === 'AM' && hours === 12) hours = 0;
+    return `${hours.toString().padStart(2, '0')}:${minutes}`;
+}
+
+function calculateTimeDifference(startTime, endTime) {
+    const [startHours, startMinutes] = startTime.split(':').map(Number);
+    const [endHours, endMinutes] = endTime.split(':').map(Number);
+    let startTotalMinutes = startHours * 60 + startMinutes;
+    let endTotalMinutes = endHours * 60 + endMinutes;
+    if (endTotalMinutes < startTotalMinutes) endTotalMinutes += 24 * 60;
+    let differenceMinutes = endTotalMinutes - startTotalMinutes;
+    const hours = Math.floor(differenceMinutes / 60);
+    const minutes = differenceMinutes % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+}
+
+function timeToPercentage(timeString) {
+    const [hours, minutes] = timeString.split(':').map(Number);
+    const totalMinutes = hours * 60 + minutes;
+    return Math.round((totalMinutes / 1440) * 10000) / 100;
+}
+
+function calculatePixelThreshold() {
+    const dimension = window.innerWidth <= 768 ? window.innerHeight : window.innerWidth;
+    state.pixelThreshold = dimension * (state.nighttime / 100); // Changed from daylight to nighttime
+}
+
+function getDistance(coords1, coords2) {
+    const R = 6371; // Radius of the Earth in km
+    const dLat = (coords2.lat - coords1.lat) * Math.PI / 180;
+    const dLon = (coords2.long - coords1.long) * Math.PI / 180;
+    const a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(coords1.lat * Math.PI / 180) * Math.cos(coords2.lat * Math.PI / 180) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return R * c;
+}
+
+//Event Handlers
+function handleResize() {
+    adjustWidth();
+    calculatePixelThreshold();
+    updateUI(); // This will handle showing/hiding the correct info based on screen size
+}
+
+function handleMouseMove(e) {
+    if (window.innerWidth <= 768) {
+        // Do nothing for mobile view
+    } else {
+        moveInfoElement(e.pageX, e.pageY);
+        updateInfo(e.pageX);
+    }
+}
+
+// UI manipulation
+function adjustWidth() {
+    const { daylight, nighttime } = state;
+    if (window.innerWidth <= 768) {
+        elements.day.style.height = `${daylight}svh`;
+        elements.night.style.height = `${nighttime}svh`;
+        elements.day.style.width = elements.night.style.width = "100%";
+    } else {
+        elements.day.style.width = `${daylight}svw`;
+        elements.night.style.width = `${nighttime}svw`;
+        elements.day.style.height = elements.night.style.height = "100%";
+    }
+}
+
+function moveInfoElement(x, y) {
+    const infoWidth = elements.info.offsetWidth;
+    const infoHeight = elements.info.offsetHeight;
+    const maxX = window.innerWidth - infoWidth;
+    const maxY = window.innerHeight - infoHeight;
+
+    // Ensure the info element stays within the viewport
+    const adjustedX = Math.min(Math.max(0, x), maxX);
+    const adjustedY = Math.min(Math.max(0, y), maxY);
+
+    TweenLite.to(elements.info, 0.3, { css: { left: adjustedX, top: adjustedY } });
 }
